3d56695b2538d02a77b96ea75f3953d0812707c5
k-mendza <karolmendza@gmail.com>
2020-06-02 23:55:20 +0200

    Work on custom validators

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,6 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Customer } from './customer';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+
+function emailMatcher(control: AbstractControl): {[key: string]: boolean } | null {
+  function inDirty() {
+    return control.get('email').dirty || control.get('confirmEmail').dirty;
+  }
+
+  function isInvalid() {
+    return control.get('email').value !== control.get('confirmEmail').value;
+  }
+  return isInvalid() ? {match: true} : null;
+}
+
+function ratingRangeValidator(minRangeValue: number, maxRangeValue: number): ValidatorFn {
+  return (control: AbstractControl): {[key: string]: boolean} | null => {
+    function isInvalid() {
+      return control.value !== null && (isNaN(control.value) || control.value < minRangeValue || control.value > maxRangeValue);
+    }
+    return isInvalid() ? {range: true} : null;
+  };
+}
 
 @Component({
   selector: 'app-customer',
@@ -15,9 +35,15 @@ export class CustomerComponent implements OnInit {
 
   ngOnInit() {
     this.customerForm = this.formBuilder.group({
-      firstName: '',
-      lastName: '',
-      email: '',
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      emailGroup: this.formBuilder.group({
+        email: ['', [Validators.required, Validators.email]],
+        confirmEmail: ['', [Validators.required, Validators.email]],
+      }),
+      phone: '',
+      notification: 'email',
+      rating: [null, ratingRangeValidator(1, 5)],
       sendCatalog: true
     });
   }
@@ -25,5 +51,20 @@ export class CustomerComponent implements OnInit {
   save() {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
+  }
+
+  isInvalid(formControlName: string): boolean {
+    return (this.customerForm.get(formControlName).touched || this.customerForm.get(formControlName).dirty)
+      && this.customerForm.get(formControlName).invalid;
   }
 }
